fed309e9bca3dadb24263fdee6da4888018d3d5b
Franchukkk <sha471251@gmail.com>
2024-04-18 18:28:55 +0300

    adaptiv

---
+++ src/js/scripts.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let lastScrollTop = 0
+    
+    if (document.querySelector(".header-line")) {
+    
+        window.addEventListener("scroll", function() {
+          let currentScroll = window.pageYOffset || document.documentElement.scrollTop
+          if (currentScroll > lastScrollTop && currentScroll > 100) {
+
+            document.querySelector('.header-line').classList.add('hide')
+          } else if (currentScroll < lastScrollTop) {
+
+            document.querySelector('.header-line').classList.remove('hide')
+          }
+          lastScrollTop = currentScroll
+        })
+    
+
+        let burger = document.querySelector(".burger"),
+            burgerMenu = document.querySelector(".top-navigation")
+            burgerBtn = document.querySelector(".burger-wrap")
+    
+            burgerBtn.addEventListener("click", function() {
+                burger.classList.toggle("active")
+                burgerMenu.classList.toggle("active")
+            })
+
+    
+    }
+
+
+})    
