a0e0966e1e1f5950f5921bd6f43c3255df1756f8
Zavorotna <zavorotnaa96@gmail.com>
2024-04-30 10:36:35 +0300

    fix

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -32,3 +32,33 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 })    
+
+
+if(document.querySelector(".slide")) {
+  const slides = document.querySelectorAll(".slide")
+  let currentSlideIndex = 0
+  
+  function changeSlide() {
+    const nextSlideIndex = (currentSlideIndex + 1) % slides.length
+    slides[currentSlideIndex].style.transform = "translateX(100%) rotateY(90deg)"
+    slides[nextSlideIndex].style.transform = "translateX(0) rotateY(0deg)"
+    currentSlideIndex = nextSlideIndex
+  }
+  
+  changeSlide()
+  
+  setInterval(changeSlide, 3000)
+
+}
+
+
+const btnReadMore = document.querySelectorAll(".readmore")
+
+    btnReadMore.forEach((item) => {
+        item.addEventListener("click", function (e) {
+          e.preventDefault()
+            const descriptionMore = item.nextElementSibling
+            descriptionMore.classList.toggle("visible")
+            item.classList.toggle("readmore-active")
+        })
+    })
