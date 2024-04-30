e8357773d4ee83ed21f3da513dcf509ba378445e
Franchukkk <sha471251@gmail.com>
2024-04-30 15:24:43 +0300

    checkboxes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -34,22 +34,7 @@ document.addEventListener("DOMContentLoaded", function() {
 })    
 
 
-if(document.querySelector(".slide")) {
-  const slides = document.querySelectorAll(".slide")
-  let currentSlideIndex = 0
-  
-  function changeSlide() {
-    const nextSlideIndex = (currentSlideIndex + 1) % slides.length
-    slides[currentSlideIndex].style.transform = "translateX(100%) rotateY(90deg)"
-    slides[nextSlideIndex].style.transform = "translateX(0) rotateY(0deg)"
-    currentSlideIndex = nextSlideIndex
-  }
-  
-  changeSlide()
-  
-  setInterval(changeSlide, 3000)
 
-}
 
 
 const btnReadMore = document.querySelectorAll(".readmore")
