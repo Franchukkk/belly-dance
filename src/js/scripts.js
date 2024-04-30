10d576cc6697fb5154ea92f691a4d509d3ce18a3
Zavorotna <zavorotnaa96@gmail.com>
2024-04-30 15:09:18 +0300

    fix

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -62,3 +62,53 @@ const btnReadMore = document.querySelectorAll(".readmore")
             item.classList.toggle("readmore-active")
         })
     })
+
+
+// select 
+const selects = document.querySelectorAll('.select')
+
+selects.forEach(select => {
+    const selectIn = select.querySelector('.select__in'),
+        selectItems = select.querySelectorAll('.select__item'),
+        thisInput = select.querySelector('.select__input'),
+        event = new Event('change')
+
+    selectIn.addEventListener('click', () => {
+        selects.forEach(_select => {
+            if (_select !== select)
+                _select.classList.remove('is-opened')
+        })
+        select.classList.toggle('is-opened')
+    })
+
+    selectItems.forEach(item => {
+        item.addEventListener('click', () => {
+            thisInput.value = item.dataset.value
+            thisInput.dispatchEvent(event)
+            selectIn.innerHTML = item.innerHTML
+            selectItems.forEach(_item => {
+                _item.classList.remove('is-active')
+            });
+            item.classList.add('is-active')
+            select.classList.remove('is-opened')
+        })
+    })
+})
+
+document.addEventListener('click', e => {
+    if (!e.target.closest('.select')) {
+        selects.forEach(select => {
+            if (select.classList.contains('is-opened'))
+                select.classList.remove('is-opened')
+        })
+    }
+})
+
+document.addEventListener('keyup', e => {
+    if (e.key == 'Escape') {
+        selects.forEach(select => {
+            if (select.classList.contains('is-opened'))
+                select.classList.remove('is-opened')
+        })
+    }
+})
