0c17cbb7d1dc6053d1a9ea179da5d97fdf4fec78
Franchukkk <sha471251@gmail.com>
2024-05-09 20:59:33 +0300

    slider

---
+++ src/js/disortion-effect.js
@@ -0,0 +1,7 @@
+// let header = document.querySelector(".header-general"),
+//     bgposX = 0
+
+// setInterval(() => {
+//     header.style.backgroundPositionX = bgposX + "px"
+//     bgposX += 1000
+// }, 1000)
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,16 +1,17 @@
-// if(document.querySelector(".slide")) {
-const slides = document.querySelectorAll(".slide")
-let currentSlideIndex = 0
+const slides = document.querySelectorAll(".slide");
+let currentSlideIndex = 0;
 
 function changeSlide() {
-    const nextSlideIndex = (currentSlideIndex + 1) % slides.length
-    slides[currentSlideIndex].style.transform = "translateX(100%) rotateY(90deg)"
-    slides[nextSlideIndex].style.transform = "translateX(0) rotateY(0deg)"
-    currentSlideIndex = nextSlideIndex
+  slides.forEach((e) => {
+    e.classList.remove("first-slide")
+  })
+  const nextSlideIndex = (currentSlideIndex + 2) % slides.length;
+  slides[currentSlideIndex].style.transform = "translateX(100%) rotateY(90deg)";
+  slides[nextSlideIndex].style.transform = "translateX(-5%) rotateY(0deg)";
+  slides[nextSlideIndex].classList.add("first-slide")
+  currentSlideIndex = (currentSlideIndex + 1) % slides.length;
 }
-
+changeSlide()
 changeSlide()
 
-setInterval(changeSlide, 3000)
-  
-// }
+setInterval(changeSlide, 3000); // Adjust the timing as needed
