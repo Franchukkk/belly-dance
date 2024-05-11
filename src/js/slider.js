70689beb72d75a4f75a0a0cb328e8d4f5a8328ff
Franchukkk <sha471251@gmail.com>
2024-05-11 16:19:11 +0300

    slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,17 +1,40 @@
 const slides = document.querySelectorAll(".slide");
-let currentSlideIndex = 0;
+let currentSlideIndex = 0
+let i = 0
 
 function changeSlide() {
   slides.forEach((e) => {
     e.classList.remove("first-slide")
   })
+
+  setTimeout(() => {
+    slides[currentSlideIndex].style.transform = "translateX(100%) rotateY(90deg)"
+  }, 2700)
   const nextSlideIndex = (currentSlideIndex + 2) % slides.length;
-  slides[currentSlideIndex].style.transform = "translateX(100%) rotateY(90deg)";
-  slides[nextSlideIndex].style.transform = "translateX(-5%) rotateY(0deg)";
-  slides[nextSlideIndex].classList.add("first-slide")
-  currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+  
+  if (i % 2 == 0) {
+    setTimeout(() => {
+      if (i !== 2) {
+
+        slides[nextSlideIndex].style.transform = "translateX(10%) rotateY(0deg)"
+
+      }
+    }, 300)
+
+  } else if (i == 0) {
+    slides[nextSlideIndex].style.transform = "translateX(10%) rotateY(0deg)"
+  }
+
+  if (i !== 1) {
+    slides[currentSlideIndex].classList.add("first-slide")
+  }
+
+  currentSlideIndex = (currentSlideIndex + 1) % slides.length
+  i++
 }
+
 changeSlide()
 changeSlide()
 
-setInterval(changeSlide, 3000); // Adjust the timing as needed
+setInterval(changeSlide, 3000)
+setInterval(changeSlide, 3000)
