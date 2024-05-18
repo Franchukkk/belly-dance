a5c1da2b42c1cbb52f8d003c4e9ccf1217dc48cd
Franchukkk <sha471251@gmail.com>
2024-05-18 13:45:40 +0300

    slider, adaptiv

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -27,6 +27,52 @@ document.addEventListener("DOMContentLoaded", function () {
             burgerMenu.classList.toggle("active")
         })
     }
+
+
+    if (document.querySelector(".cross")) {
+        // alert("dsa")
+        let closeFilter = document.querySelector(".cross"),
+            filter = document.querySelector(".filter"),
+            filterOpen = document.querySelector(".filter-open")
+
+        function filterToggle() {
+            filter.classList.toggle("d-block")
+        }
+        closeFilter.addEventListener("click", filterToggle)
+        filterOpen.addEventListener("click", filterToggle)
+    }
+
+    if (document.querySelector(".slide")) {
+
+        const slides = document.querySelectorAll(".slide"),
+            timeBetweenSwitch = 300 // різниця в часі між двома слайдами в перемиканні
+        let currentSlideIndex = 0,
+            i = 0
+
+        function changeSlide() {
+            slides.forEach((e) => {
+                if (e.classList.contains("first-slide")) {
+                    setTimeout(() => {
+                        e.classList.remove("first-slide")
+                    }, timeBetweenSwitch)
+                } else if (e.classList.contains("second-slide")) {
+                    e.classList.remove("second-slide")
+                }
+            })
+
+            slides[currentSlideIndex].classList.add("first-slide")
+            setTimeout(() => {
+                slides[currentSlideIndex - 1 == -1 ? slides.length - 1 : currentSlideIndex - 1].classList.add("second-slide")
+            }, timeBetweenSwitch)
+
+            currentSlideIndex = currentSlideIndex < slides.length - 2 ? currentSlideIndex += 2 : 0
+
+        }
+
+        setInterval(changeSlide, 3000)
+
+    }
+
 })
 
 const btnReadMore = document.querySelectorAll(".readmore")
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,72 +1,30 @@
-<<<<<<< HEAD
-if(document.querySelector(".slide")) {
+// if (document.querySelector(".slide")) {
 
-  const slides = document.querySelectorAll(".slide");
-  let currentSlideIndex = 0
-  let i = 0
+//   const slides = document.querySelectorAll(".slide"),
+//     timeBetweenSwitch = 300 // різниця в часі між двома слайдами в перемиканні
+//   let currentSlideIndex = 0,
+//     i = 0
   
-  function changeSlide() {
-    slides.forEach((e) => {
-      e.classList.remove("first-slide")
-    })
+//   function changeSlide() {
+//     slides.forEach((e) => {
+//       if (e.classList.contains("first-slide")) {
+//         setTimeout(() => {
+//           e.classList.remove("first-slide")
+//         }, timeBetweenSwitch)
+//       } else if (e.classList.contains("second-slide")) {
+//         e.classList.remove("second-slide")
+//       }
+//     })
   
-    setTimeout(() => {
-      slides[currentSlideIndex].style.transform = "translateX(100%) rotateY(90deg)"
-    }, 2700)
-    const nextSlideIndex = (currentSlideIndex + 2) % slides.length;
+//     slides[currentSlideIndex].classList.add("first-slide")
+//     setTimeout(() => {
+//       slides[currentSlideIndex - 1 == -1 ? slides.length - 1 : currentSlideIndex - 1].classList.add("second-slide")
+//     }, timeBetweenSwitch)
+  
+//     currentSlideIndex = currentSlideIndex < slides.length - 2  ? currentSlideIndex += 2 : 0
     
-    if (i % 2 == 0) {
-      setTimeout(() => {
-        if (i !== 2) {
+//   }
   
-          slides[nextSlideIndex].style.transform = "translateX(10%) rotateY(0deg)"
-  
-        }
-      }, 300)
-  
-    } else if (i == 0) {
-      slides[nextSlideIndex].style.transform = "translateX(10%) rotateY(0deg)"
-    }
-  
-    if (i !== 1) {
-      slides[currentSlideIndex].classList.add("first-slide")
-    }
-  
-    currentSlideIndex = (currentSlideIndex + 1) % slides.length
-    i++
-  }
-  
-  changeSlide()
-  changeSlide()
-  
-  setInterval(changeSlide, 3000)
-  setInterval(changeSlide, 3000)
-}
-=======
-const slides = document.querySelectorAll(".slide"),
-  timeBetweenSwitch = 300 // різниця в часі між двома слайдами в перемиканні
-let currentSlideIndex = 0,
-  i = 0
+//   setInterval(changeSlide, 3000)
 
-function changeSlide() {
-  slides.forEach((e) => {
-    if (e.classList.contains("first-slide")) {
-      setTimeout(() => {
-        e.classList.remove("first-slide")
-      }, timeBetweenSwitch)
-    } else if (e.classList.contains("second-slide")) {
-      e.classList.remove("second-slide")
-    }
-  })
-
-  slides[currentSlideIndex].classList.add("first-slide")
-  setTimeout(() => {
-    slides[currentSlideIndex - 1 == -1 ? slides.length - 1 : currentSlideIndex - 1].classList.add("second-slide")
-  }, timeBetweenSwitch)
-
-  currentSlideIndex = currentSlideIndex < slides.length - 2  ? currentSlideIndex += 2 : 0
-  
-}
-
-setInterval(changeSlide, 3000)
->>>>>>> a7649c1627730dbf158d2382edd8ff1f7e04c671
+// }
