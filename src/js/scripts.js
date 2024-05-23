a1a743d2b35d11ea8d74427cfd5698267ef78679
Franchukkk <sha471251@gmail.com>
2024-05-23 19:20:22 +0300

    правки

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,20 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
     let lastScrollTop = 0
 
     if (document.querySelector(".header-line")) {
-        if (window.innerWidth < 800) {
-            window.addEventListener("scroll", function () {
-                let currentScroll = window.pageYOffset || document.documentElement.scrollTop
-                if (currentScroll > lastScrollTop && currentScroll > 100) {
 
-                    document.querySelector('.header-line').classList.add('hide')
-                } else if (currentScroll < lastScrollTop) {
+        window.addEventListener("scroll", function () {
+            let currentScroll = window.pageYOffset || document.documentElement.scrollTop
+            if (currentScroll > lastScrollTop && currentScroll > 100) {
 
-                    document.querySelector('.header-line').classList.remove('hide')
-                }
-                lastScrollTop = currentScroll
-            })
+                document.querySelector('.header-line').classList.add('hide')
+            } else if (currentScroll < lastScrollTop) {
 
-        }
+                document.querySelector('.header-line').classList.remove('hide')
+            }
+            lastScrollTop = currentScroll
+        })
+
+
 
 
         let burger = document.querySelector(".burger"),
