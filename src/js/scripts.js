4b779d7d0f33a34b67c06b2f6c39642bae9a1c58
Franchukkk <sha471251@gmail.com>
2024-06-08 12:55:38 +0300

    map, scroll

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -21,10 +21,21 @@ document.addEventListener("DOMContentLoaded", function () {
         let burger = document.querySelector(".burger"),
             burgerMenu = document.querySelector(".top-navigation")
         burgerBtn = document.querySelector(".burger-wrap")
-
-        burgerBtn.addEventListener("click", function () {
+        
+        function burgerToggle() {
             burger.classList.toggle("active")
             burgerMenu.classList.toggle("active")
+        }
+
+        burgerBtn.addEventListener("click", burgerToggle)
+
+        let scrollLinks = document.querySelectorAll(".scroll-link")
+
+        scrollLinks.forEach((el) => {
+            el.addEventListener("click", function(e) {
+                burgerToggle()
+
+            })
         })
     }
 
